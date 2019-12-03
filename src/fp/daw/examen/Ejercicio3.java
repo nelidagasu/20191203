f93ad2158c7b39d88456d80d1a5818b0a8cab0fe
alumno <alumno@A209P51.informatica.cifpaviles.pa>
2019-12-03 17:38:31 +0100

    Ejercicio 3 del examen resuelto

--- src/fp/daw/examen/Ejercicio3.java
+++ src/fp/daw/examen/Ejercicio3.java
@@ -1,5 +1,7 @@
 package fp.daw.examen;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	/* 3 puntos
@@ -13,7 +15,50 @@ public class Ejercicio3 {
 	 */
 	
 	public static void main(String[] args) {
+		int numero;
+		int suma = 0;
+		int contador = 0;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número para saber si es guay:");
+		numero = sc.nextInt();
+		
+		switch (numero) {
+			case 0:
+				System.out.println("El número 0 no es un número guay.");
+				break;
+			case 1:
+				System.out.println("El número 1 no es un número guay.");
+				break;
+			default:				
+				for (int i = 1; i < numero; i++) {
+//					System.out.println("i " + i);
+					suma = i;
+					contador = 1;
+					
+					for (int j = i + 1; j < numero; j++) {
+						suma = suma + j; 
+//						System.out.println("j " + j + " suma " + suma + " contador " + contador);
+						
+						contador ++;
+						
+						if (suma == numero)
+							break;
+					}	
+					
+//					System.out.println("i " + i + " suma " + suma + " contador " + contador);
+					
+					if (suma == numero)
+						break;
+				}				
+//				System.out.println("fuera suma " + suma + " contador " + contador);
+		}
+		
+		if (contador > 1 & suma == numero)
+			System.out.println("El número " + numero + " es un número guay.");
+		else
+			System.out.println("El número " + numero + " no es un número guay.");
 	}
 
 }
